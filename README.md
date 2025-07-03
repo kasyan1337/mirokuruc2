676585170594907cf38de7fbd23a591952877860
Miro Kuruc <miroslav.kuruc01@gmail.com>
2025-07-03 18:34:41 +0200

    Deployment of personal website

---
+++ README.md
@@ -0,0 +1,20 @@
+# whoami
+
+This website acts as a digital business card, allowing visitors to learn more about me, my writing, and easily access my contact information. Simple, clean, and minimalistic design created with lot of coffee.
+
+## 📂 Project Structure
+```
+/ (Root)
+├── blog/           # File for blogposts
+├── pictures/       # Images, icons, and other resources
+├── CNAME           # Domain config
+├── README.md       # Project documentation
+├── index.html      # Main webpage
+├── resume.html     # Resume webpage
+├── robot.txt       # Web crawlers restricion
+├── script.js       # Java scripts
+└── style.css       # .css Style
+```
+
+## 🤝 Contributing
+Feel free to fork this repository and submit pull requests if you'd like to improve or personalize it for yourself!
---
+++ script.js
@@ -0,0 +1,97 @@
+function randomChar() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&";
+    return chars[Math.floor(Math.random() * chars.length)];
+}
+
+function generateRandomText(length) {
+    return Array.from({length}, () => randomChar()).join("");
+}
+
+function animateText(element, targetText, callback) {
+    let iterations = 0;
+    const interval = setInterval(() => {
+        element.innerText = targetText
+            .split("")
+            .map((char, index) => {
+                if (index < iterations) return char;
+                return randomChar();
+            })
+            .join("");
+        if (iterations >= targetText.length) {
+            clearInterval(interval);
+            if (callback) callback();
+        }
+        iterations += 1;
+    }, 50);
+}
+
+    document.querySelectorAll('.encrypted-text').forEach((el) => {
+        const originalText = el.getAttribute('data-original');
+        el.textContent = originalText;
+        el.addEventListener('mouseenter', () => {
+            animateText(el, generateRandomText(originalText.length));
+        });
+        el.addEventListener('mouseleave', () => {
+            animateText(el, originalText);
+        });
+    });
+
+    const download = document.getElementById('downloadLink');
+    if (download) {
+        download.addEventListener('click', function (e) {
+            e.preventDefault();
+            var fileUrl = this.href;
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', fileUrl, true);
+            xhr.responseType = 'blob';
+            xhr.onload = function () {
+                var blob = xhr.response;
+                var link = document.createElement('a');
+                link.href = URL.createObjectURL(blob);
+                link.download = 'Miro Kuruc (65C4EF4B) – Public PGP Key.asc';
+                link.click();
+            };
+            xhr.send();
+        });
+    }
+
+    const profilePicture = document.querySelector('.profile-picture img');
+    if (profilePicture) {
+        document.addEventListener('mousemove', (event) => {
+            const {clientX, clientY} = event;
+            const {left, top, width, height} = profilePicture.getBoundingClientRect();
+            const centerX = left + width / 2;
+            const centerY = top + height / 2;
+            const deltaX = clientX - centerX;
+            const deltaY = clientY - centerY;
+            const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+            profilePicture.style.transform = `rotate(${angle}deg)`;
+        });
+        profilePicture.addEventListener('mouseleave', () => {
+            profilePicture.style.transform = 'rotate(0deg)';
+        });
+    }
+
+    const arrow = document.querySelector('.scroll-arrow');
+    if (arrow) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 60) {
+                arrow.style.opacity = '0';
+                arrow.style.bottom = '0px';
+            } else {
+                arrow.style.opacity = '1';
+                arrow.style.bottom = '40px';
+            }
+        });
+    }
+
+function toggleList(listId) {
+    var list = document.getElementById(listId);
+    if (list) {
+        if (list.style.display === 'none' || list.style.display === '') {
+            list.style.display = 'block';
+        } else {
+            list.style.display = 'none';
+        }
+    }
+}
